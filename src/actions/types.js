1848e9288788c7ab358a889a879ef5d9a68078f3
Cristian Plesca <cplesca@zaizi.com>
2021-06-25 14:38:32 +0100

    New types for fetching users

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,8 @@ export const LOGIN_USER = "login_user";
 export const AUTH_FAILED = "auth_failed";
 export const USERNAME_CHANGED = "username_changed";
 export const PASSWORD_CHANGED = "password_changed";
+
+// Users Types
+export const FETCH_USERS = "fetch_users";
+export const INITIALIZE_FETCH = "initialize_fetch"
+export const ERROR_FETCHING_USERS = "error_fetching_users";
