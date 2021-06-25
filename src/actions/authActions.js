93f0849e486fb14fa1f42406a594bbcbc1e800e2
Cristian Plesca <cplesca@zaizi.com>
2021-06-25 11:25:20 +0100

    New action passwordChanged, reformatted types import

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,6 +1,16 @@
-import { LOGIN_USER, AUTH_FAILED, USERNAME_CHANGED } from "./types";
+import {
+  LOGIN_USER,
+  AUTH_FAILED,
+  USERNAME_CHANGED,
+  PASSWORD_CHANGED,
+} from "./types";
 
 export const usernameChanged = (text) => ({
   type: USERNAME_CHANGED,
+  payload: text,
+});
+
+export const passwordChanged = (text) => ({
+  type: PASSWORD_CHANGED,
   payload: text,
 });
