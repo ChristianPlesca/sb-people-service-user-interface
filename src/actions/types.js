34ec9c952fc517d8e168cb5d51528f734a4b6e02
Cristian Plesca <cplesca@zaizi.com>
2021-06-25 11:25:58 +0100

    Username type fixed, new type password_changed

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 // Auth Types
 export const LOGIN_USER = "login_user";
 export const AUTH_FAILED = "auth_failed";
-export const USERNAME_CHANGED = "user_name_changed";
+export const USERNAME_CHANGED = "username_changed";
+export const PASSWORD_CHANGED = "password_changed";
